1720bf4d1c6c92e6e157da402185a0626e2482da
Rokulus <76742480+Rokulus@users.noreply.github.com>
2022-04-15 21:30:57 +0200

    feat: add async version

---
+++ 08_async.py
@@ -0,0 +1,34 @@
+"""
+Jakub Prôčka
+08 async
+"""
+import time
+import asyncio
+from aiohttp import ClientSession
+
+
+async def jokes(names):
+    x = names.split()
+    async with ClientSession() as session:
+        async with session.get('http://api.icndb.com/jokes/random?firstName='
+                               + x[0] + '&lastName=' + x[1]) as response:
+            print_json(await response.json())
+
+
+def print_json(data):
+    print('\n------------------------------')
+    print('Joke: ', data['value']['joke'])
+    print('Categories of the joke: ', data['value']['categories'])
+    print('------------------------------')
+
+
+async def main():
+    names = ['jozko paprika', 'peto zemiak', 'miso uhorka', 'palo paradajka']
+
+    start = time.time()
+    await asyncio.gather(*(jokes(name) for name in names))
+    end = time.time()
+    print('Time elapsed:', end - start)
+
+if __name__ == '__main__':
+    asyncio.run(main())
